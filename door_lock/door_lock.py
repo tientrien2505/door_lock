29b104f67f0d9162a963d064cb4ff9abcf708197
tientrien2505 <tientrien2505@gmail.com>
2021-04-19 15:24:01 +0700

    use add_event_detect instead of threading

--- door_lock/door_lock.py
+++ door_lock/door_lock.py
@@ -1,6 +1,5 @@
 import Jetson.GPIO as GPIO
 from time import sleep
-from threading import Thread
 import os
 import logging
 from dotenv import load_dotenv, find_dotenv
@@ -16,15 +15,16 @@ else:
 try:
     BUTTON_PIN = int(os.getenv('BUTTON_PIN')) if os.getenv('BUTTON_PIN') is not None else 11
     BOLT_PIN = int(os.getenv('BOLT_PIN')) if os.getenv('BOLT_PIN') is not None else 15
-    DURATION_PRESS_OF_BUTTON = float(os.getenv('DURATION_PRESS_OF_BUTTON')) if os.getenv('DURATION_PRESS_OF_BUTTON') is not None else 0.04
+    DURATION_PRESS_OF_BUTTON = int(os.getenv('DURATION_PRESS_OF_BUTTON')) if os.getenv('DURATION_PRESS_OF_BUTTON') is not None else 0.04
     logging.info('loaded environment variables successfully')
 except:
     logging.warning('loading environment variables is fail')
     BUTTON_PIN = 11
     BOLT_PIN = 15
-    DURATION_PRESS_OF_BUTTON = 0.04
+    DURATION_PRESS_OF_BUTTON = 40
 
-class DoorLock(Thread):
+# class DoorLock(Thread):
+class DoorLock():
 
     def __init__(self, daemon=False, button_pin=BUTTON_PIN, bolt_pin=BOLT_PIN, duration=DURATION_PRESS_OF_BUTTON):
         try:
@@ -33,27 +33,27 @@ class DoorLock(Thread):
             self.bolt_pin = bolt_pin
             self.duration = duration
             
-            self._deactivate = False
             GPIO.setup(self.button_pin, GPIO.IN)
             GPIO.setup(self.bolt_pin, GPIO.OUT)
-            super().__init__(daemon=daemon)
-            self.start()
+            GPIO.add_event_detect(self.button_pin, GPIO.RISING, self.__callback, self.duration)
+            self._deactivate = False
             logging.info("door lock system started")
         except Exception as ex:
             logging.error('could not start door lock system')
             raise Exception(ex)
     
-    def run(self):
-        self.process_button()
 
     def deactivate(self):
-        self._deactivate = True
+        GPIO.remove_event_detect(self.button_pin)
+    
+    def __callback(self, channel):
+        self.open_door()
     
     def activate(self):
         if self._deactivate is True:
             try:
                 self._deactivate = False
-                self.start()
+                GPIO.add_event_detect(self.button_pin, GPIO.RISING, self.__callback, self.duration)
                 logging.info('door lock started')
             except:
                 self._deactivate = True
@@ -61,17 +61,6 @@ class DoorLock(Thread):
         else:
             logging.info('door lock system is still running')
     
-    def process_button(self):
-        try:
-            while True:
-                value = GPIO.input(self.button_pin)
-                if value == GPIO.LOW:
-                    self.open_door()
-                if self._deactivate:
-                    break
-                sleep(self.duration)
-        except:
-            GPIO.cleanup()
     
     def open_door(self):
         GPIO.output(self.bolt_pin, GPIO.HIGH)
@@ -81,5 +70,5 @@ class DoorLock(Thread):
 
 if __name__ == '__main__':
     dl = DoorLock()
-    sleep(2)
-    dl.open_door()
+    while True:
+        sleep(1000)
